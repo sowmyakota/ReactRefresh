1d7730e6c656e03fd4dccff7b85741a8f9f57247
“sowmyakota” <venkatasowmyasrikota98@gmail.com>
2023-01-08 03:20:50 -0600

    Added form and controlling input

--- Section5/reactwithapi/src/components/SearchBar.js
+++ Section5/reactwithapi/src/components/SearchBar.js
@@ -1,11 +1,21 @@
+import { useState } from "react";
+
 function SearchBar({ onSubmit }) {
-  const handleClick = () => {
-    onSubmit("cars");
+  const [term, setTerm] = useState();
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    onSubmit(term);
+  };
+
+  const handleChange = (event) => {
+    setTerm(event.target.value);
   };
 
   return (
     <div>
-      <input />
+      <form onSubmit={handleFormSubmit}>
+        <input onChange={handleChange} />
+      </form>
     </div>
   );
 }
