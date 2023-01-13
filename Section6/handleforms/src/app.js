ac8e469a191c4cd0b0926ea6e18744fdca3e5405
“sowmyakota” <venkatasowmyasrikota98@gmail.com>
2023-01-12 23:06:51 -0600

    Added Books List Component

--- Section6/handleforms/src/app.js
+++ Section6/handleforms/src/app.js
@@ -1,15 +1,21 @@
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
 
 function App() {
   const [books, setBooks] = useState([]);
   const createBook = (title) => {
-    //creating new array instaed of mutating it.
-    const updatedBook = [...books, { id: 123, title: title }];
+    //creating new array instead of mutating it.
+    const updatedBook = [...books, { id: Math.random() * 9999, title: title }];
     setBooks(updatedBook);
     console.log("Need to add book with", title);
   };
-  return <BookCreate onCreate={createBook} />;
+  return (
+    <div>
+      <BookList books={books} />
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 }
 
 export default App;
--- Section6/handleforms/src/components/BookShow.js
+++ Section6/handleforms/src/components/BookShow.js
@@ -1,5 +1,5 @@
-const BookShow = () => {
-  return <div>BookEdit</div>;
+const BookShow = ({ book }) => {
+  return <div className="book-show">{book.title}</div>;
 };
 
 export default BookShow;
