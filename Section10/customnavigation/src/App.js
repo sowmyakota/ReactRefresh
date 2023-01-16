c0afbd8884099b3a38aaa3b7fa208e2ae56574c6
“sowmyakota” <venkatasowmyasrikota98@gmail.com>
2023-01-16 17:47:54 -0600

    Adding icons

--- Section10/customnavigation/src/App.js
+++ Section10/customnavigation/src/App.js
@@ -1,26 +1,36 @@
 import Button from "./Button";
+import { GoGear, GoZap, GoSync, GoReport, GoRuby } from "react-icons/go";
 
 function App() {
   return (
     <div>
       <div>
-        <Button secondary>Click me!</Button>
+        <Button secondary outline>
+          <GoGear />
+          Click me!
+        </Button>
       </div>
       <div>
         <Button danger outline>
+          <GoZap />
           Buy Now!
         </Button>
       </div>
       <div>
-        <Button warning>See Deal!</Button>
+        <Button warning>
+          <GoSync />
+          See Deal!
+        </Button>
       </div>
       <div>
         <Button secondary outline>
+          <GoReport />
           Hide Ads!
         </Button>
       </div>
       <div>
         <Button success rounded>
+          <GoRuby />
           Hide Ads!
         </Button>
       </div>
