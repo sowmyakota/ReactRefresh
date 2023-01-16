3b87000be5c0709383a81926c9d31756f1c3ed5f
“sowmyakota” <venkatasowmyasrikota98@gmail.com>
2023-01-16 13:06:31 -0600

    Added button component

--- Section10/customnavigation/src/Button.js
+++ Section10/customnavigation/src/Button.js
@@ -9,7 +9,11 @@ function Button({
   outline,
   rounded,
 }) {
-  return <button>{children}</button>;
+  return (
+    <button className="px-3 py-1.5 border border-blue-600 bg-blue-500 text-white">
+      {children}
+    </button>
+  );
 }
 
 Button.propTypes = {
