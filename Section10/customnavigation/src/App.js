d638b26ad0eb4ceab2820ace2ea32dce6f11f324
“sowmyakota” <venkatasowmyasrikota98@gmail.com>
2023-01-17 11:01:46 -0600

    Added toggle collapse

--- Section10/customnavigation/src/App.js
+++ Section10/customnavigation/src/App.js
@@ -4,22 +4,26 @@ function App() {
     {
       id: "abc",
       label: "Can I use React on a project",
-      content: "You can use react on any project",
+      content:
+        "You can use react on any project. You can use react on any project. You can use react on any project. You can use react on any project",
     },
     {
       id: "bcd",
       label: "Can I use Javascript on a project",
-      content: "You can use Javascript on any project",
+      content:
+        "You can use Javascript on any project. You can use Javascript on any project. You can use Javascript on any project. You can use Javascript on any project.",
     },
     {
       id: "cde",
-      label: "Can i use Css on a project",
-      content: "You can use Css on any project",
+      label: "Can I use Css on a project",
+      content:
+        "You can use Css on any project. You can use Css on any project. You can use Css on any project. You can use Css on any project",
     },
     {
       id: "def",
-      label: "Can i use tailwind on a project",
-      content: "You can use tailwind on any project",
+      label: "Can I use tailwind on a project",
+      content:
+        "You can use tailwind on any project. You can use tailwind on any project. You can use tailwind on any project. You can use tailwind on any project.",
     },
   ];
 
--- Section10/customnavigation/src/components/Accordion.js
+++ Section10/customnavigation/src/components/Accordion.js
@@ -1,13 +1,38 @@
+import { useState } from "react";
+import { GoChevronLeft, GoChevronDown } from "react-icons/go";
 function Accordion({ items }) {
-  const renderedItems = items.map((item) => {
+  const [expandedIndex, setExpandedIndex] = useState(-1);
+
+  const handleClick = (nextIndex) => {
+    if (expandedIndex === nextIndex) {
+      setExpandedIndex(-1);
+    } else {
+      setExpandedIndex(nextIndex);
+    }
+  };
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
+    const icon = (
+      <span className="text-xl">
+        {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+      </span>
+    );
+
     return (
       <div key={item.id}>
-        <div>{item.label}</div>
-        <div>{item.content}</div>
+        <div
+          className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer"
+          onClick={() => handleClick(index)}
+        >
+          {item.label}
+          {icon}
+        </div>
+        {isExpanded && <div className="border-b p-5">{item.content}</div>}
       </div>
     );
   });
-  return <div>{renderedItems}</div>;
+  return <div className="border-x border-t rounded">{renderedItems}</div>;
 }
 
 export default Accordion;
